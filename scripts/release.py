27f9b1d027e40755ad6c4fa39e723906936696e6
thanhtoan1742 <thanhtoan1742@gmail.com>
2024-08-13 14:12:43 +0700

    Fixup release.py

--- scripts/release.py
+++ scripts/release.py
@@ -1,19 +1,18 @@
-import os
-import sys
 import json
+import os
 import pathlib
+import sys
 
 import requests
 
+BASE_URL = "https://api.github.com"
 GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 GIT_TAG = os.environ["GIT_TAG"]
 
 assert len(GIT_TAG), "Git tag is empty"
 assert len(GITHUB_TOKEN), "Github token is empty"
 
-print(GIT_TAG)
-
-BASE_URL = "https://api.github.com"
+print("Downloading turing_segment with tag:", GIT_TAG)
 
 
 def is_release_asset(a):
@@ -21,18 +20,15 @@ def is_release_asset(a):
     return name.startswith("turing_segment") and name.endswith(".whl")
 
 
-
 r = requests.get(
     f"{BASE_URL}/repos/bioturing-org/modeling/releases/tags/{GIT_TAG}",
-    headers = {
+    headers={
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "X-GitHub-Api-Version": "2022-11-28",
-    }
+    },
 )
 
-print(r.text)
 assert r.status_code == 200, f"Cannot get release base on tag, tag = {GIT_TAG}"
-
 
 
 assets = [
@@ -44,10 +40,12 @@ assets = [
 ]
 
 
-assets = list(filter(
-    is_release_asset,
-    assets,
-))
+assets = list(
+    filter(
+        is_release_asset,
+        assets,
+    )
+)
 
 print(json.dumps(assets, indent=2))
 assert len(assets) > 0, "Assets is empty"
@@ -62,11 +60,11 @@ for asset in assets:
     print(f"Downloading {name}")
     r = requests.get(
         f"{BASE_URL}/repos/bioturing-org/modeling/releases/assets/{id}",
-        headers = {
+        headers={
             "Accept": "application/octet-stream",
             "Authorization": f"Bearer {GITHUB_TOKEN}",
             "X-GitHub-Api-Version": "2022-11-28",
-        }
+        },
     )
 
     with open(assets_dir / name, "wb+") as f:
